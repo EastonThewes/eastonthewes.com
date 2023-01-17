fe2862782b6e5cf1c7d03bf0e979e5f3ba7c4203
Easton Thewes <theweseaston@gmail.com>
2023-01-17 16:32:41 -0500

    does this pic work?

--- eastonthewes.com/components/HomePagePicture.vue
+++ eastonthewes.com/components/HomePagePicture.vue
@@ -2,7 +2,7 @@
 
         <div class="pt-8 pb-24 md:pt-0 flex justify-center">
             <div>
-                <img src="" alt="Easton" class="w-80 md:w-96 rounded-3xl drop-shadow-2xl shadow-2xl"/>
+                <img src="../assets/easton.jpg" alt="Easton" class="w-80 md:w-96 rounded-3xl drop-shadow-2xl shadow-2xl"/>
             </div>
         </div>
 
