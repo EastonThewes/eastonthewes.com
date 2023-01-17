f235a34c4446af6353046cd7aa8f38e05e14bea4
Easton Thewes <theweseaston@gmail.com>
2023-01-17 16:11:48 -0500

    Static Target

--- eastonthewes.com/nuxt.config.ts
+++ eastonthewes.com/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-    modules: [    '@nuxtjs/tailwindcss'  ]
+// export default defineNuxtConfig({
+//     modules: [    '@nuxtjs/tailwindcss'  ]
+// })
 
-})
+export default {
+    target:'static',
+    modules: [    '@nuxtjs/tailwindcss'  ]
+}
+
+
