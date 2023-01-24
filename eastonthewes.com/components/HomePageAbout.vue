920aaa6a33ecbd8c5dfa896be56b04a835d6027f
Easton Thewes <theweseaston@gmail.com>
2023-01-24 16:49:24 -0500

    Changed colors for website

--- eastonthewes.com/components/HomePageAbout.vue
+++ eastonthewes.com/components/HomePageAbout.vue
@@ -1,14 +1,14 @@
 <template>
     
         <div class="md:mx-8" >
-            <h2 class="text-7xl md:text-8xl font-semibold pb-8 drop-shadow-xl md:text-left text-center easton">hi, i'm <b class="text-green-500">easton</b></h2>
+            <h2 class="text-7xl md:text-8xl font-semibold pb-8 drop-shadow-xl md:text-left text-center easton">hi, i'm <b class="text-amber-400">easton</b></h2>
             <p class="text-2xl md:text-4xl font-extralight drop-shadow-2xl about  ">I am a recent graduate from West Virginia University and I am looking for a full time position!</p>
         </div>
         <div class="flex gap-12 text-4xl pt-8 justify-center md:justify-start md:ml-8">
-            <a href="https://github.com/eastonthewes" class="hover:text-6xl hover:text-green-500 ease-in-out duration-75"><i class="bi bi-github" title="My Github!"></i></a>    
-            <a href="https://www.linkedin.com/in/eastonthewes/" class="hover:text-6xl hover:text-green-500 ease-in-out duration-75"><i class="bi bi-linkedin" title="My LinkedIn!"></i></a>
-            <a href="mailto: theweseaston@gmail.com" class="hover:text-6xl hover:text-green-500 ease-in-out duration-75"><i class="bi bi-envelope" title="Email Me!"></i></a>
-            <a href="https://storage.googleapis.com/eastonthewes.com/ThewesEaston_Resume.pdf" download class="hover:text-6xl hover:text-green-500 ease-in-out duration-75"><i class="bi bi-filetype-pdf" title="My Resume!"></i></a>
+            <a href="https://github.com/eastonthewes" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-github" title="My Github!"></i></a>    
+            <a href="https://www.linkedin.com/in/eastonthewes/" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-linkedin" title="My LinkedIn!"></i></a>
+            <a href="mailto: theweseaston@gmail.com" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-envelope" title="Email Me!"></i></a>
+            <a href="https://storage.googleapis.com/eastonthewes.com/ThewesEaston_Resume.pdf" download class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-filetype-pdf" title="My Resume!"></i></a>
         </div>
 
 
