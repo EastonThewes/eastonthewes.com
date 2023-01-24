c8734003258bfff8616ca579653d5ecb7fdcee46
Easton Thewes <theweseaston@gmail.com>
2023-01-24 16:58:22 -0500

    Added content for experience page

--- eastonthewes.com/pages/xp.vue
+++ eastonthewes.com/pages/xp.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-
+    <div class="pt-16 bg-secondary">
+        <XPCard 
+        title="Application Developer"
+        company="Encova Insurance"
+        startDate="May 2022"
+        endDate="December 2022" 
+        desc1="Designed and implemented front-end solutions using the Angular framework in combination with TypeScript, C# and the .NET framework to automate regulatory file processing and add quality of life features for internal tool users"
+        desc2="Evaluated existing applications and provided updates and modifications based on business needs"
+        desc3="Provided user support through ServiceNow for interal web applications"/>
+        <!-- <XPCard title="TBA" company="Your Company" startDate="soon" endDate="N/A"></XPCard> -->
     </div>
 </template>
 
 <script>
+    import XPCard from '../components/XPCard.vue'
     export default {
-        
+        components: {
+            XPCard
+        },
+        data() {
+            return {     
+                    title:'',
+                    company:'',
+                    startDate:'',
+                    endDate:'',
+                    desc1:'',
+                    desc2:'',
+                    desc3:''
+
+            }
+        }
     }
 </script>
 
