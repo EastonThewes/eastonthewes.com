1a51d620c828ccdcb81385704bd81bcdeea0c4af
EastonThewes <theweseaston@gmail.com>
2023-01-30 13:37:07 -0500

    added target for about me links

--- eastonthewes.com/components/HomePageAbout.vue
+++ eastonthewes.com/components/HomePageAbout.vue
@@ -5,10 +5,10 @@
             <p class="text-2xl md:text-4xl font-extralight drop-shadow-2xl about  ">I am a recent graduate from West Virginia University and I am looking for a full time position!</p>
         </div>
         <div class="flex gap-12 text-4xl pt-8 justify-center md:justify-start md:ml-8">
-            <a href="https://github.com/eastonthewes" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-github" title="My Github!"></i></a>    
-            <a href="https://www.linkedin.com/in/eastonthewes/" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-linkedin" title="My LinkedIn!"></i></a>
-            <a href="mailto: theweseaston@gmail.com" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-envelope" title="Email Me!"></i></a>
-            <a href="https://storage.googleapis.com/eastonthewes.com/ThewesEaston_Resume.pdf" download class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-filetype-pdf" title="My Resume!"></i></a>
+            <a href="https://github.com/eastonthewes" target="_blank" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-github" title="My Github!"></i></a>    
+            <a href="https://www.linkedin.com/in/eastonthewes/" target="_blank" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-linkedin" title="My LinkedIn!"></i></a>
+            <a href="mailto: theweseaston@gmail.com" target="_blank" class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-envelope" title="Email Me!"></i></a>
+            <a href="https://storage.googleapis.com/eastonthewes.com/ThewesEaston_Resume.pdf" target="_blank" download class="hover:text-6xl hover:text-amber-400 ease-in-out duration-75"><i class="bi bi-filetype-pdf" title="My Resume!"></i></a>
         </div>
 
 
