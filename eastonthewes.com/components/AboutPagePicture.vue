3b95c3f87f67d83b4eef65bb149e7507ddb0ee85
EastonThewes <theweseaston@gmail.com>
2023-02-07 13:49:45 -0500

    Added content for about page

---
+++ eastonthewes.com/components/AboutPagePicture.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex w-3/6 " id="AboutPagePicture">
+        <div class="flex justify-center">
+            <img class="rounded-3xl" :src=" filename ">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            filename: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
