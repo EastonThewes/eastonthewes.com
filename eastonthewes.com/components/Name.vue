414433a40e092aa36f937ef515b59e89c63bb537
EastonThewes <theweseaston@gmail.com>
2023-03-02 22:49:52 -0500

    fixed image

--- eastonthewes.com/components/Name.vue
+++ eastonthewes.com/components/Name.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex gap-4 justify-center h-min min-w-full">
-        <img class="image" src="../assets/images/easton.JPEG" alt="">
+        <img class="image" src="../assets/images/eastonnew.jpg" alt="">
         <div class="flex flex-col justify-center">
             <div class="name">
                 Easton Thewes
@@ -25,6 +25,8 @@
     overflow: hidden;
     border-radius: 50%;
 }
+
+
 
 .sub-text{
     color:gray;
